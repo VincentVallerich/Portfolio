a0592bc29391c8bad51def396901daa453af579d
VincentVallerich <vallerichvincent@outlook.fr>
2020-03-02 19:08:36 +0100

    Add 7 in 1 folders and nav bar

---
+++ script.js
@@ -0,0 +1,15 @@
+const switchButton = document.querySelector(".button");
+const bodyColor = document.body.style.backgroundColor;
+let toggle = 1;
+
+switchButton.addEventListener("click", function() {
+  if (toggle) {
+    document.body.style.backgroundColor = "#2c3e50";
+    toggle++;
+    console.log("dark");
+  } else {
+    document.body.style.backgroundColor = "#eee";
+    toggle--;
+    console.log("white");
+  }
+});
